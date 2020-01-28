b54e6db5816f02bae53048a745973679a566958e
EzeVolyBlu <ezequieel.blum@gmail.com>
2020-01-28 17:37:03 -0300

    second version

---
+++ keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+    mongoURI: "mongodb+srv://ezeVolyBlu:lm76wzbqXX7avZwW@mycluster-36rt5.gcp.mongodb.net/myTineraries?retryWrites=true&w=majority",
+    secretOrKey: "secret",
+    GOOGLE_CLIENT_ID: "690443591935-0r5mr57pbfk330telig3t5pscdb3pk8q.apps.googleusercontent.com",
+    GOOGLE_CLIENT_SECRET: "cK-oqciifSoIZ96wBt_oVkhx"
+  };
+
